172160e3c68fbf785e658342d8d80662d2306c06
meng <wulalameng@gmail.com>
2017-09-05 15:19:29 -0400

    create TodoList Component

---
+++ src/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Todo from './Todo';
+
+const TodoList = ({todos, onTodoClick}) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo
+        key={todo.id}
+        text={todos.text}
+        completed={todo.completed}
+        onClick={()=>onTodoClick(todo.id)}
+      />
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shapeOf({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired
+    })
+  ),
+  onTodoClick: PropTypes.func.isRequired
+};
+
+export default TodoList;
